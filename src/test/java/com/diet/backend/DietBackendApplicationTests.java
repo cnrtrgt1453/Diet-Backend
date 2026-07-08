d601c616f2a901af3a498b65eeda96b7399a3301
Caner <caner@dietapp.com>
2026-07-08 14:33:30 +0300

    init: backend spring boot project

---
+++ src/test/java/com/diet/backend/DietBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diet.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
